744c9a52f45481ff3ed104743c3a9e9a40621164
Chibundu <ndubest56@gmail.com>
2023-05-17 13:42:36 +0100

    profile lookup function finalized

---
+++ study.js
@@ -0,0 +1,13 @@
+/*for (let i = 0; i < 20; i++){
+	console.log(Math.random());
+}
+for (let i = 0; i < 20; i++){
+	console.log(Math.random() * 20);
+}
+*/
+for (let i = 0; i < 20; i++){
+	console.log(Math.ceil(Math.random() * 10));
+}
+for (let i = 0; i < 20; i++){
+	console.log(Math.floor(Math.random() * (20 -10 + 1))+10 );
+}
