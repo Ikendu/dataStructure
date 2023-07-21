a5a889b3186346cd7a0ac1a5d85c4f911e5ff1dc
Aniede Chibundu <ndubest56@gmail.com>
2023-07-21 04:43:29 +0100

    documentation and 0 or more match

--- regex.js
+++ regex.js
@@ -1,13 +1,18 @@
 const myString =
   "I am becoming a skillful option programmer, Now I can do more programming I can do 1, 2, 3, 4 to 9";
-const myregex = /skillful/i;
-const aregex = /program/gi;
-const moregex = /[a-z0-9]/gi;
+const myregex = /skillful/i; //.........i to match upper and lower case
+const aregex = /program/gi; //..........g to match more than one
+const moregex = /[a-z0-9]/gi; //........match all alphabets and numbers
+const findMore = /m+/gi; //.............match one or more times
+const zeroOrMore = /am+/gi; //...........zero or more times
 
 const result = myregex.test(myString);
 const output = myString.match(myregex);
 const output2 = myString.match(aregex);
-console.log(result);
-console.log(output);
-console.log(output2);
-console.log(myString.match(moregex));
+const output3 = myString.match(findMore);
+const output4 = myString.match(zeroOrMore);
+// console.log(result);
+// console.log(output);
+// console.log(output2);
+// console.log(myString.match(moregex));
+console.log(output4);
