c17e554d16d0275d86f143d293c5c41d5943f63e
Aniede Chibundu <ndubest56@gmail.com>
2023-07-21 09:27:13 +0100

    shorthand for all alphanumeric and non-numeric characters

--- regex.js
+++ regex.js
@@ -3,9 +3,11 @@ const myString =
 const myregex = /skillful/i; //.........i to match upper and lower case
 const aregex = /program/gi; //..........g to match more than one
 const moregex = /[a-z0-9]/gi; //........match all alphabets and numbers
+const shortHand = /\w/gi; //............match all characters and numbers shorthand
+const shortHand2 = /\W/g; //............match all non-alphanumelic shorthand
 const findMore = /m+/gi; //.............match one or more times
-const zeroOrMore = /am+/gi; //..........zero or more times
-const negate = /[^sop]/gi; //...........things that will not be matched
+const zeroOrMore = /am*/gi; //.......... * to match zero or more times
+const negate = /[^sop]/gi; //...........^ things that will not be matched
 
 const result = myregex.test(myString);
 const output = myString.match(myregex);
