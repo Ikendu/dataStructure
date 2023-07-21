5504b2a24e2747afd008214b5edbcb6f1e01a85e
Aniede Chibundu <ndubest56@gmail.com>
2023-07-21 10:40:29 +0100

    username format included

--- regex.js
+++ regex.js
@@ -4,17 +4,22 @@ const myregex = /skillful/i; //.........i to match upper and lower case
 const aregex = /program/gi; //..........g to match more than one
 const moregex = /[a-z0-9]/gi; //........match all alphabets and numbers
 const shortHand = /\w/gi; //............match all characters and numbers shorthand
-const shortHand2 = /\W/g; //............match all non-alphanumelic shorthand
+const shortHand2 = /\W/g; //............match all non-alphanumeric shorthand
 const findMore = /m+/gi; //.............match one or more times
 const zeroOrMore = /am*/gi; //.......... * to match zero or more times
 const negate = /[^sop]/gi; //...........^ things that will not be matched
+const numeric = /\d/g; //................match all digits
+
+const username = /^[a-z][a-z]+\d*$|^[a-z]+\d\d+$/i; //username must start with a letter and end
+// with a digit, the first two must be a letter, it can be upper or lower case, if it contains
+// only one letter it must be accompanied with two numbers
 
 const result = myregex.test(myString);
 const output = myString.match(myregex);
 const output2 = myString.match(aregex);
 const output3 = myString.match(findMore);
 const output4 = myString.match(zeroOrMore);
-const output5 = myString.match(negate);
+const output5 = myString.match(shortHand2);
 // console.log(result);
 // console.log(output);
 // console.log(output2);
