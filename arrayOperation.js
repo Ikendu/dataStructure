7c5d2e4c8ca60c5eaaedd6926fb524bd122b4701
Aniede Chibundu <ndubest56@gmail.com>
2023-07-22 07:40:21 +0100

    array adding and removing function

---
+++ arrayOperation.js
@@ -0,0 +1,18 @@
+const arrAdd = (arr, items) => {
+  arr.unshift(items);
+  arr.push(items);
+  return arr;
+};
+const remove = (arr) => {
+  let pushed = arr.shift();
+  let popped = arr.pop();
+
+  console.log(pushed, popped, arr);
+};
+const myArr = ["hello", "success", "come", "here"];
+let things = "welcome";
+
+// console.log(myArr);
+// arrAdd(myArr, things);
+// console.log(myArr);
+remove(myArr);
