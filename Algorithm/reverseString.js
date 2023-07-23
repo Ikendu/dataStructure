e13540e8ab62e1f804fd9d5a09e429425b75400e
Aniede Chibundu <ndubest56@gmail.com>
2023-07-23 17:22:11 +0100

    string reverse usein array methods

--- Algorithm/reverseString.js
+++ Algorithm/reverseString.js
@@ -1,9 +1,18 @@
+//using iteration with for-loop
 const reverseStr = (str) => {
   let revStr = "";
   for (let i = str.length - 1; i >= 0; i--) {
-    revStr = +str[i];
+    revStr += str[i];
   }
   return revStr;
 };
 
-console.log(reverseStr(David));
+console.log(reverseStr("David"));
+
+//using array method
+const revString = (str) => {
+  newStr = str.split("").reverse().join("");
+  return newStr;
+};
+
+console.log(revString("Another David"));
