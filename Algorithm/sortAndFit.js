b73c8b18eb75fd1885f651c8e445d7ea7a6abdfc
Aniede Chibundu <ndubest56@gmail.com>
2023-07-25 14:16:19 +0100

    added findIndex and ternay operator

--- Algorithm/sortAndFit.js
+++ Algorithm/sortAndFit.js
@@ -1,4 +1,4 @@
-//using sort and for loop
+//1, using sort and for loop
 const getIndexToIns = (arr, num) => {
   arr.sort((a, b) => a - b);
 
@@ -11,9 +11,26 @@ const getIndexToIns = (arr, num) => {
 };
 console.log(getIndexToIns([2, 5, 10], 15));
 
-//using filter and length method
+//2, using filter and length methods
 
 const insertAtIndex = (arr, num) => {
   return arr.filter((val) => num > val).length;
 };
 console.log(insertAtIndex([2, 5, 10], 15));
+
+//3, using concate, sort and indexOf
+
+const insertInIndex = (arr, num) => {
+  return arr
+    .concat(num)
+    .sort((a, b) => a - b)
+    .indexOf(num);
+};
+console.log(insertInIndex([2, 5, 10], 15));
+
+//4, using findIndex and ternary operator
+const insertItem = (arr, num) => {
+  let index = arr.sort((a, b) => a - b).findIndex((val) => val >= num);
+  return index === -1 ? arr.length : index;
+};
+console.log(insertItem([2, 5, 10], 15));
