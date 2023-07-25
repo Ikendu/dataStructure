e9a5ee573fa81008152d56513fa6dac0c6c8ba03
Aniede Chibundu <ndubest56@gmail.com>
2023-07-25 16:11:53 +0100

    checking wether a string in contained in another

---
+++ Algorithm/indexOf.js
@@ -0,0 +1,18 @@
+const checkInput = (arr) => {
+  let main = arr[0].toLowerCase();
+  let test = arr[1].toLowerCase();
+
+  for (let i = 0; i < test.length; i++) {
+    if (main.indexOf(test) === -1) return false;
+  }
+  return true;
+};
+console.log(checkInput(["ManboyI", "boyi"]));
+
+const testAdjacent = (arr) => {
+  let main = arr[0].toLowerCase();
+  let test = arr[1].toLowerCase();
+
+  return test.split("").every((letter) => main.indexOf(letter) !== -1);
+};
+console.log(testAdjacent(["ManboyI", "boyi"]));
