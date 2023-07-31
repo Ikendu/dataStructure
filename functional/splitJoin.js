54fecce53cde60f38c6ffc56a45b8514cd6f68b2
Aniede Chibundu <ndubest56@gmail.com>
2023-07-31 05:53:48 +0100

    make correct sentence

--- functional/splitJoin.js
+++ functional/splitJoin.js
@@ -3,3 +3,7 @@ const spliter = (arr) => {
   arr.split(/\d/); //split by digit
   arr.split(/\W/); //split by non alphanumeric
 };
+
+const sentence = (arr) => {
+  return arr.split(/\W/).join(" "); //split by non-alphanumeric and join by space to form a sentence
+};
