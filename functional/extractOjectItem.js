48ae2f8369301b69472022347a7bc41333afd53d
Aniede Chibundu <ndubest56@gmail.com>
2023-08-01 23:53:35 +0100

    extracting items from array collection

--- functional/extractOjectItem.js
+++ functional/extractOjectItem.js
@@ -16,3 +16,24 @@ console.log(
     { last: "Capulet" }
   )
 );
+
+//addin .map and reduce
+const extractItem = (arr, match) => {
+  let sourceKey = Object.keys(match);
+
+  return arr.filter((obj) =>
+    sourceKey
+      .map((key) => obj.hasOwnProperty(key) && obj[key] === match[key])
+      .reduce((a, b) => a && b)
+  );
+};
+console.log(
+  extractItem(
+    [
+      { first: "Romeo", last: "Montague" },
+      { first: "Mercutio", last: null },
+      { first: "Tybalt", last: "Capulet" },
+    ],
+    { last: "Capulet" }
+  )
+);
