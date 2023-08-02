21f5e7e0663e5b518b5014cf0f5db97976d17e25
Aniede Chibundu <ndubest56@gmail.com>
2023-08-02 10:52:47 +0100

    matching and replacing with -

--- functional/regexMatch.js
+++ functional/regexMatch.js
@@ -4,7 +4,26 @@
 const replace = (str) => {
   return str
     .replace(/([a-z])([A-Z])/g, "$1 $2") //use "$1 $2" selector to put a space between lower-Upper case
-    .replace(/\s+|_/g, "-") //replace all space and underscore with hyphen;
+    .replace(/\s|_/g, "-") //replace all space and underscore with hyphen;
     .toLowerCase(); //turn all to lower-case
 };
 console.log(replace("He is comming_soon DontGoThere"));
+
+//using split and join
+const matchRegx = (str) => {
+  return str
+    .split(/\s|_|(?=[A-Z])/) //split at whitespace, underscore, tailing upperCase
+    .join("-") //join split with hyphen
+    .toLowerCase(); //turn to lowercase
+};
+console.log(matchRegx("He is comming_soon DontGoThere"));
+
+//using split and join II
+const regexMatch = (str) => {
+  return str
+    .replace(/([a-z])([A-Z])/g, "$1 $2")
+    .split(/_| /)
+    .join("-")
+    .toLowerCase();
+};
+console.log(regexMatch("He is comming_soon DontGoThere"));
