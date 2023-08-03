d952cbe6513a516fdc491a4e20b7ca4a42c9670d
Aniede Chibundu <ndubest56@gmail.com>
2023-08-03 08:28:58 +0100

    using regex to check case

--- functional/replaceWordInSentence.js
+++ functional/replaceWordInSentence.js
@@ -12,3 +12,20 @@ const myReplace = (sentence, word, replacer) => {
 console.log(
   myReplace("A quick brown fox Jumped over the lazy dog", "Jumped", "leaped")
 );
+
+//using regex to check
+const myReplacer = (sentence, word, replacer) => {
+    if (word.match(/^[A-Z]/))
+}
+
+//using regex to check for uppercase
+const myReplace = (str, word, replacer) => {
+	if(word.match(/[a-z]/)){
+		replacer = replacer.charAt(0).toUpperCase() + replacer.slice(1)
+	} else {
+		replacer = replacer.charAt(0).toLowerCase() + replacer.slice(1)
+	}
+	return str.replace(word, replacer)
+}
+console.log(myReplace("A quick brown fox Jumped over the lazy dog",
+ "Jumped", "leaped"));
