6659461f88a63a17d2ef7bf9a2fe7529b52324c8
Aniede Chibundu <ndubest56@gmail.com>
2023-08-03 08:34:59 +0100

    brief documentation

--- functional/replaceWordInSentence.js
+++ functional/replaceWordInSentence.js
@@ -1,3 +1,4 @@
+//using indexOf method to check upper case
 const myReplace = (sentence, word, replacer) => {
   let index = sentence.indexOf(word);
 
@@ -14,28 +15,27 @@ console.log(
 );
 
 //using regex to check
-const myReplacer = (sentence, word, replacer) => {
-    if (word.match(/^[A-Z]/))
-}
 
 //using regex to check for uppercase
-const myReplace = (str, word, replacer) => {
-	if(word.match(/[a-z]/)){
-		replacer = replacer.charAt(0).toUpperCase() + replacer.slice(1)
-	} else {
-		replacer = replacer.charAt(0).toLowerCase() + replacer.slice(1)
-	}
-	return str.replace(word, replacer)
-}
-console.log(myReplace("A quick brown fox Jumped over the lazy dog",
- "Jumped", "leaped"));
-
- //using array index
 const myReplacer = (str, word, replacer) => {
-	if (word[0] === word[0].toUpperCase()){
-		replacer = replacer.replace(replacer[0], replacer[0].toUpperCase())
-	}
-	return str.replace(word, replacer)
-}
-console.log(myReplacer("A quick brown fox Jumped over the lazy dog",
- "Jumped", "leaped"));
+  if (word.match(/[a-z]/)) {
+    replacer = replacer.charAt(0).toUpperCase() + replacer.slice(1);
+  } else {
+    replacer = replacer.charAt(0).toLowerCase() + replacer.slice(1);
+  }
+  return str.replace(word, replacer);
+};
+console.log(
+  myReplacer("A quick brown fox Jumped over the lazy dog", "Jumped", "leaped")
+);
+
+//using array index
+const wordReplace = (str, word, replacer) => {
+  if (word[0] === word[0].toUpperCase()) {
+    replacer = replacer.replace(replacer[0], replacer[0].toUpperCase());
+  }
+  return str.replace(word, replacer);
+};
+console.log(
+  wordReplace("A quick brown fox Jumped over the lazy dog", "Jumped", "leaped")
+);
