57502016041a93887a8b95825510b34588b41ef3
Aniede Chibundu <ndubest56@gmail.com>
2023-08-04 20:54:26 +0100

    return set with no duplicate

---
+++ functional/setOfArr.js
@@ -0,0 +1,13 @@
+function getSet(arr) {
+  let newArr = [];
+
+  for (let i = 0; i < arguments.length; i++) {
+    for (let j = 0; j < arguments[i].length; j++) {
+      if (newArr.indexOf(arguments[i][j]) < 0) {
+        newArr.push(arguments[i][j]);
+      }
+    }
+  }
+  return newArr;
+}
+console.log(getSet([1, 3, 2], [5, 2, 1, 4], [2, 1]));
