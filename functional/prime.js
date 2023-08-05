62b98d16c853da14434d23efad1cb259faaf0715
Aniede Chibundu <ndubest56@gmail.com>
2023-08-05 11:24:30 +0100

    get prime numbers

---
+++ functional/prime.js
@@ -0,0 +1,16 @@
+//get all prime numbers up to num
+const getPrime = (num) => {
+  const isPrime = (num) => {
+    let sqrt = Math.sqrt(num);
+    for (let i = 2; i <= sqrt; i++) {
+      if (num % i === 0) return false;
+    }
+    return true;
+  };
+  let sum = 0;
+  for (let i = 2; i < num; i++) {
+    if (isPrime(i)) sum += i;
+  }
+  return sum;
+};
+console.log(getPrime(10));
