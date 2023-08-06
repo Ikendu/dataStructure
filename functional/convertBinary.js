d96ec2a23bd5d7bb913e7d08e88925fe6f23103f
Aniede Chibundu <ndubest56@gmail.com>
2023-08-06 08:12:24 +0100

    convert binary to wirds

---
+++ functional/convertBinary.js
@@ -0,0 +1,14 @@
+const binaryStr = (str) => {
+  let myStr = str.split(" ");
+  let newStr = [];
+
+  for (let i = 0; i < str.length; i++) {
+    newStr.push(String.fromCharCode(parseInt(myStr[i], 2)));
+  }
+  return newStr.join("");
+};
+console.log(
+  binaryStr(
+    "01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"
+  )
+);
