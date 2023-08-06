ddbd06651d4aa0404941a908975741c6b5e6a903
Aniede Chibundu <ndubest56@gmail.com>
2023-08-06 10:18:16 +0100

    item truth checker

--- functional/objectCheck.js
+++ functional/objectCheck.js
@@ -1,6 +1,7 @@
 const truthCheck = (arr, item) => {
   let counter = 0;
   for (c in arr) {
+    //using for-in
     if (arr[c].hasOwnProperty(item) && Boolean(arr[c][item])) {
       counter++;
     }
@@ -9,6 +10,30 @@ const truthCheck = (arr, item) => {
 };
 console.log(
   truthCheck(
+    [
+      { name: "Quincy", role: "Founder", isBot: false },
+      { name: "Naomi", role: "", isBot: false },
+      { name: "Camperbot", role: "Bot", isBot: true },
+    ],
+    "name"
+  )
+);
+//using traditional for-loop and only Boolean checker
+const itemChecker = (arr, item) => {
+  let counter = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (Boolean(arr[i][item])) {
+      counter++;
+    }
+  }
+  if (counter === arr.length) {
+    return "The property item is found in all";
+  } else {
+    return "It is not complete";
+  }
+};
+console.log(
+  itemChecker(
     [
       { name: "Quincy", role: "Founder", isBot: false },
       { name: "Naomi", role: "", isBot: false },
