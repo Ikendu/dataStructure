e27a8b06787c1f83193438df0178ff85f220ebd3
Aniede Chibundu <ndubest56@gmail.com>
2023-08-07 13:16:49 +0100

    handling formular

--- functional/formulaFunc.js
+++ functional/formulaFunc.js
@@ -41,3 +41,27 @@ const getPeriod = (arr) => {
   return newArr;
 };
 console.log(getPeriod([{ name: "moon", avgAlt: 378632.553 }]));
+
+//using JSON.parse, JSON.stringify and forEach
+const periodOrbit = (arr) => {
+  const GM = 398600.4418;
+  const eR = 6367.4447;
+  const pi = 2 * Math.PI;
+  let newArr = JSON.parse(JSON.stringify(arr));
+
+  newArr.forEach((item) => {
+    const getOrbit = Math.round(
+      2 * Math.PI * Math.sqrt(Math.pow(eR + item.avgAlt, 3) / GM)
+    );
+    delete item.avgAlt;
+    item.periodOrbit = getOrbit;
+  });
+  return newArr;
+};
+console.log(
+  periodOrbit([
+    { name: "iss", avgAlt: 413.6 },
+    { name: "hubble", avgAlt: 556.7 },
+    { name: "moon", avgAlt: 378632.553 },
+  ])
+);
