0c818e4035d392456e9f861f7f34990f518f24f7
Aniede Chibundu <ndubest56@gmail.com>
2023-08-07 18:52:30 +0100

    palindrome using reverse method

--- functional/palindrome.js
+++ functional/palindrome.js
@@ -8,3 +8,10 @@ const palindrome = (str) => {
   return true;
 };
 console.log(palindrome("never odd or even"));
+
+//using split-reverse-join method
+const palindrom = (str) => {
+  let newStr = str.toLowerCase().replace(/[\W_]/g, "");
+  return newStr === newStr.split("").reverse().join("");
+};
+console.log(palindrom("never odd or even"));
