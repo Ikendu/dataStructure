d0e97283180c2ee176f1d21aa5491baf4fcac794
Aniede Chibundu <ndubest56@gmail.com>
2023-08-13 12:56:04 +0100

    num rotate by a given mun

--- Algorithm/Test/rotate.js
+++ Algorithm/Test/rotate.js
@@ -1,3 +1,20 @@
-const rotate = (arr) => {
-  newArr = [];
+const rotate = (arr, k) => {
+  let newArr = [];
+  let temp = [];
+
+  if (k < 0) {
+    return arr;
+  }
+
+  for (let i = 0; i < arr.length - k; i++) {
+    newArr.push(arr[i]);
+  }
+  for (let i = arr.length - k; i < arr.length; i++) {
+    temp.unshift(arr[i]);
+  }
+  for (let i = 0; i < temp.length; i++) {
+    newArr.unshift(temp[i]);
+  }
+  console.log(newArr);
 };
+rotate([-1, -100, 3, 99], 2);
