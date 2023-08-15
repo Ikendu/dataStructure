a38e500e31dc22d0bd6b9cf993c43908f291852a
Aniede Chibundu <ndubest56@gmail.com>
2023-08-15 08:09:37 +0100

    finding longest string

---
+++ Algorithm/Revision/longestStr.js
@@ -0,0 +1,23 @@
+const longest = (str) => {
+  let newStr = str.split(" ");
+  let maxLength = 0;
+
+  for (let i = 0; i < newStr.length; i++) {
+    if (maxLength < newStr[i].length) {
+      maxLength = newStr[i].length;
+    }
+  }
+  //   console.log(str);
+  //   console.log(newStr);
+  return maxLength;
+};
+
+console.log(longest("The quick brown fox jumpe88d over the lazy dog"));
+
+//using reduced
+const longestWord = (str) => {
+  return str
+    .split(" ")
+    .reduce((longest, word) => Math.max(longest, word.length), 0);
+};
+console.log(longestWord("The quick brown fox jumped over the lazy dog"));
