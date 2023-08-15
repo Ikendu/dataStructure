fed8ff3e843e15aa372776ce0ca000b23b6e6dcc
Aniede Chibundu <ndubest56@gmail.com>
2023-08-15 22:56:00 +0100

    truncate word

--- Algorithm/Revision/truncateStr.js
+++ Algorithm/Revision/truncateStr.js
@@ -7,3 +7,8 @@ const truncate = (str, num) => {
 };
 console.log(truncate("hello how are u doing", 15));
 console.log(truncate("My wife will give birth to a boy", 50));
+
+const truncater = (str, num) => {
+  return str.length > num ? str.slice(0, num) + "..." : str;
+};
+console.log(truncater("hello how are u doing", 15));
