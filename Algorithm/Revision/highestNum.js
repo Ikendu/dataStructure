1beedc558fde2264d6f9eba41b652cfe7c27c539
Aniede Chibundu <ndubest56@gmail.com>
2023-08-15 15:02:02 +0100

    getting highest num using for-loop

---
+++ Algorithm/Revision/highestNum.js
@@ -0,0 +1,23 @@
+const highestNum = (arr) => {
+  let result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let largerNum = arr[i][0];
+    for (let j = 1; j < arr[i].length; j++) {
+      if (largerNum < arr[i][j]) {
+        largerNum = arr[i][j];
+      }
+    }
+    console.log(largerNum);
+    result[i] = largerNum;
+  }
+  return result;
+};
+console.log(
+  highestNum([
+    [4, 5, 1, 3],
+    [13, 27, 18, 26],
+    [32, 35, 37, 39],
+    [1000, 1001, 857, 1],
+  ])
+);
