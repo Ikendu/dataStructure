0b8f55b542be61ae93c9a13897b504ae8d44172d
Aniede Chibundu <ndubest56@gmail.com>
2023-08-16 09:29:33 +0100

    copy arr to arr without mutation

--- Algorithm/Revision/copyArrToArr.js
+++ Algorithm/Revision/copyArrToArr.js
@@ -1,10 +1,34 @@
+//using slice and spread
 const copyArr = (arr1, arr2, n) => {
   let front = arr2.slice(0, n);
   let back = arr2.slice(n);
-  return [...front, ...arr2, ...back];
+  return [...front, ...arr1, ...back];
+};
+
+//using splice and spread
+const copyArr1 = (arr1, arr2, n) => {
+  let local = [...arr2];
+  local.splice(n, 0, ...arr1);
+  return local;
+};
+
+const copyArr2 = (arr1, arr2, n) => {
+  let local = arr2.slice(0);
+  for (let i = 0; i < arr1.length; i++) {
+    local.splice(n, 0, arr1[i]);
+    n++;
+  }
+  return local;
+  //console.log(local);
 };
 
 console.log(copyArr([1, 2, 3], [4, 5, 6], 1));
 console.log(
   copyArr(["claw", "tentacle"], ["head", "shoulders", "knees", "toes"], 2)
 );
+
+console.log(copyArr1([1, 2, 3], [4, 5, 6], 1));
+console.log(
+  copyArr1(["claw", "tentacle"], ["head", "shoulders", "knees", "toes"], 2)
+);
+console.log(copyArr2([1, 2, 3], [4, 5, 6], 1));
