f442ee4b0ffb6db5259cb5d3aa1cdcca44b863f2
Aniede Chibundu <ndubest56@gmail.com>
2023-08-16 11:17:30 +0100

    truthy checker

--- Algorithm/Revision/checktruthy.js
+++ Algorithm/Revision/checktruthy.js
@@ -14,8 +14,6 @@ console.log(
 );
 //if(Boolean(newArr[i]) == true) will work
 const truthy1 = (arr) => {
-  return arr(Boolean);
+  return arr.filter(Boolean);
 };
-console.log(
-  truthy1([false, null, 0, NaN, undefined, "", 7, "ate", "", false, 9])
-);
+console.log(truthy1([false, 7, "ate", "", false, 9]));
