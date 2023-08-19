a0cd49f30d6e9b85077a4d343846052cebc5c6ae
Aniede Chibundu <ndubest56@gmail.com>
2023-08-19 10:19:32 +0100

    using the filter and the map method

---
+++ Algorithm/Revision/mapFilterReduce.js
@@ -0,0 +1,15 @@
+//return the square of positive integers
+const squareList = (arr) => {
+  return arr
+    .filter((num) => num > 0 && parseInt(num) / num === 1)
+    .map((num) => num * num);
+};
+console.log(squareList([4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2]));
+
+//using modules
+const squareList1 = (arr) => {
+  return arr
+    .filter((num) => num > 0 && parseInt(num) % num === 0)
+    .map((num) => Math.pow(num, 2));
+};
+console.log(squareList1([4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2]));
