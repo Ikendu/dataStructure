0580725c0e16a4dc3f4dcd2d8c94916ab2f48f8c
Aniede Chibundu <ndubest56@gmail.com>
2023-08-20 05:28:00 +0100

    find obj in and array of objects

---
+++ Algorithm/Revision/findObjInArr.js
@@ -0,0 +1,40 @@
+//using for-loop iteration
+const findObj = (arr, obj) => {
+  let newArr = [];
+
+  for (let i in arr) {
+    let matched = false;
+    for (let j in obj) {
+      if (arr[i][j] !== obj[j]) matched = true;
+    }
+    if (!matched) newArr.push(arr[i]);
+  }
+  return newArr;
+};
+console.log(
+  findObj([{ apple: 1, bat: 2 }, { bat: 2 }, { apple: 1, bat: 2, cookie: 2 }], {
+    apple: 1,
+    bat: 2,
+  })
+);
+
+//using filter and for-loop
+const findObj2 = (arr, obj) => {
+  let objKeys = Object.keys(obj);
+
+  return arr.filter((item) => {
+    for (let i in objKeys) {
+      if (item[objKeys[i]] !== obj[objKeys[i]]) return false;
+    }
+    return true;
+  });
+};
+console.log(
+  findObj2(
+    [{ apple: 1, bat: 2 }, { bat: 2 }, { apple: 1, bat: 2, cookie: 2 }],
+    {
+      apple: 1,
+      bat: 2,
+    }
+  )
+);
