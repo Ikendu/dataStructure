4e04b95e60173f9bc6e2075b6d14cabef4f9799c
Aniede Chibundu <ndubest56@gmail.com>
2023-08-20 05:42:28 +0100

    finding obj in an arry of objects

--- Algorithm/Revision/findObjInArr.js
+++ Algorithm/Revision/findObjInArr.js
@@ -38,3 +38,19 @@ console.log(
     }
   )
 );
+
+//using .filter and .every
+const findObj1 = (arr, obj) => {
+  let objKeys = Object.keys(obj);
+
+  return arr.filter((item) => objKeys.every((ele) => item[ele] === obj[ele]));
+};
+console.log(
+  findObj1(
+    [{ apple: 1, bat: 2 }, { bat: 2 }, { apple: 1, bat: 2, cookie: 2 }],
+    {
+      apple: 1,
+      bat: 2,
+    }
+  )
+);
