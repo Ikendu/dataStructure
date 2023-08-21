e982572ad25f27466ae9ee70ec29bf813111ceae
Aniede Chibundu <ndubest56@gmail.com>
2023-08-21 10:47:35 +0100

    replace string with another

--- Algorithm/Revision/replace.js
+++ Algorithm/Revision/replace.js
@@ -10,3 +10,15 @@ const replacer = (str, before, after) => {
 console.log(
   replacer("A quick brown fox lumped over the lazy dog", "lumped", "Leaped")
 );
+
+const replacer1 = (str, before, after) => {
+  if (before[0] == before[0].toUpperCase()) {
+    after = after[0].toUpperCase() + after.slice(1);
+  } else {
+    after = after[0].toLowerCase() + after.slice(1);
+  }
+  return str.replace(before, after);
+};
+console.log(
+  replacer1("A quick brown fox Jumped over the lazy dog", "Jumped", "leaped")
+);
