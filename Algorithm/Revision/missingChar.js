dff223e2421cd4979f88d9f76351c2029d0b1b17
Aniede Chibundu <ndubest56@gmail.com>
2023-08-21 16:40:18 +0100

    get missing character

--- Algorithm/Revision/missingChar.js
+++ Algorithm/Revision/missingChar.js
@@ -13,8 +13,7 @@ console.log(missingChar("abcdefg"));
 //another form
 const missingChar1 = (str) => {
   let result = "complete range";
-
-  for (let i = 0; i < str.length; i++) {
+  for (let i = 1; i < str.length; i++) {
     if (str.charCodeAt(i) !== str.charCodeAt(i - 1) + 1) {
       result = String.fromCharCode(str.charCodeAt(i - 1) + 1);
     }
@@ -22,3 +21,33 @@ const missingChar1 = (str) => {
   return result;
 };
 console.log(missingChar1("abcdefg"));
+
+//count string from 0
+const missingChar2 = (str) => {
+  let result = "Complete";
+
+  for (let i in str) {
+    let coded = str.charCodeAt(i);
+    if (coded !== str.charCodeAt(0) + i) {
+      result = String.fromCharCode(coded - 1);
+    }
+  }
+  return result;
+};
+console.log(missingChar2("abcdeg"));
+
+//split and forEach
+const missingChar3 = (str) => {
+  let result = "Non missing";
+  let sequence = str.charCodeAt(0);
+
+  str.split("").forEach((letter) => {
+    if (letter.charCodeAt(0) === sequence) {
+      sequence++;
+    } else {
+      result = String.fromCharCode(sequence);
+    }
+  });
+  return result;
+};
+console.log(missingChar3("abcdefg"));
