1f45d8d6cc918e51145dfe736b2a0f5938c1f5bb
Aniede Chibundu <ndubest56@gmail.com>
2023-08-22 06:57:58 +0100

    html convrter

--- Algorithm/Revision/replaceHtml.js
+++ Algorithm/Revision/replaceHtml.js
@@ -1,3 +1,4 @@
+//convert the strinh to html type
 const htmlReplace = (str) => {
   let converter = {
     "&": "&amp;",
@@ -7,6 +8,40 @@ const htmlReplace = (str) => {
     "<>": "&lt;&gt;",
     "'": "&apos;",
   };
-  return str.replace(/[&<>"']+/g, (i) => converter[i]);
+  return str.replace(/[&<>"']/g, (i) => converter[i]);
 };
-console.log(htmlReplace("Dolce <> 'Gabbana"));
+console.log(htmlReplace("Dolce <> Gabbana"));
+
+//using the map method
+const htmlReplace1 = (str) => {
+  let converter = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': "&quot;",
+    "<>": "&lt;&gt;",
+    "'": "&apos;",
+  };
+  return str
+    .split("")
+    .map((item) => converter[item] || item)
+    .join("");
+};
+console.log(htmlReplace1("Dolce <> Gabbana"));
+
+//using for-in
+const htmlReplace2 = (str) => {
+  let converter = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': "&quot;",
+    "<>": "&lt;&gt;",
+    "'": "&apos;",
+  };
+  for (let i in converter) {
+    str = str.replace(new RegExp(i, "g"), converter[i]);
+  }
+  return str;
+};
+console.log(htmlReplace2("Dolce <> Gabbana"));
