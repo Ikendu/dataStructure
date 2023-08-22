c8bd9197351945323631bc13ceb9b992fb728a89
Aniede Chibundu <ndubest56@gmail.com>
2023-08-22 16:22:45 +0100

    array flattening

--- Algorithm/Revision/flattend.js
+++ Algorithm/Revision/flattend.js
@@ -10,3 +10,17 @@ const arrFlat = (arr) => {
   return flatted;
 };
 console.log(arrFlat([1, [2], {}, [], [3, [[4]]]]));
+
+//another way
+const arrFlat1 = (arr) => {
+  return arr
+    .toString()
+    .replace(",,", ",")
+    .split(",")
+    .map((val) => {
+      if (val == "[object Object]") return {};
+      else if (isNaN(val)) return val;
+      else return parseInt(val);
+    });
+};
+console.log(arrFlat1([1, [2], {}, [], [3, [[4]]]]));
