8d7900b1fc7e1e62986e9f1cc76618308f691263
Aniede Chibundu <ndubest56@gmail.com>
2023-08-23 07:13:25 +0100

    incomplete arguments

--- Algorithm/Revision/argMissing.js
+++ Algorithm/Revision/argMissing.js
@@ -1,4 +1,4 @@
-//using spread arguments
+//using spread arguments and typeof keyword
 const addMissing = (...args) => {
   if (args.length > 2) return "only two numbers are needed";
   let [a, b] = [...args];
@@ -14,3 +14,21 @@ const addMissing = (...args) => {
   }
 };
 console.log(addMissing(2, 3, 4));
+
+//using arguments method and isInteger keyword
+function addMissing1() {
+  let [a, b] = arguments;
+
+  const isNum = (num) => {
+    return Number.isInteger(num);
+  };
+  if (isNum(a)) {
+    if (isNum(b)) return a + b;
+    if (!b) {
+      return (b) => {
+        return addMissing(a, b); //OR return a + b
+      };
+    }
+  }
+}
+console.log(addMissing1(2, 33));
