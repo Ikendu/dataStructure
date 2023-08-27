450fbe312332a82b4193f645b8203a98c5b6be26
Aniede Chibundu <ndubest56@gmail.com>
2023-08-27 16:08:56 +0100

    cecears cifer word converion

--- functional/wordCode.js
+++ functional/wordCode.js
@@ -2,9 +2,38 @@ const wordCode = (str) => {
   let input = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
   let output = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-  let index = (x) => input.indexOf(x);
-  let trans = (x) => (index(x) > -1 ? output[index(x)] : x);
-
-  return str.split("").map(trans).join("");
+  return str
+    .split("")
+    .map((item) => {
+      let index = input.indexOf(item);
+      if (index > -1) {
+        return output[index];
+      } else {
+        return item;
+      }
+    })
+    .join("");
 };
-console.log(wordCode("SERR PBQR PNZC"));
+console.log(wordCode("SERR PBQR PNZC!"));
+//let test = [4, 5, 6, 3, 9];
+//console.log(test.indexOf(9));
+
+//using character code and regex to test alphabet
+const wordCode1 = (str) => {
+  let result = [];
+
+  return str
+    .split("")
+    .map((item) => {
+      if (/[A-Z]/.test(item)) {
+        let code = item.charCodeAt() + (/[A-M]/.test(item) ? +13 : -13);
+        return String.fromCharCode(code);
+      } else {
+        return item;
+      }
+    })
+    .join("");
+
+  //return result.join("");
+};
+console.log(wordCode1("SERR PBQR PNZC!"));
