e3a73bed8711fc215b3eadffcbdafd793393ff3b
Chibunud <ndubest56@gmail.com>
2024-01-17 07:45:05 +0100

    longest array

--- 01 FreshStart/slidingWindow.js
+++ 01 FreshStart/slidingWindow.js
@@ -13,17 +13,42 @@
 // }
 // console.log(ans)
 
-let arry2 = `1101100011`
-let left = 0,
-  currZeros = 0,
-  ans = 0
-for (let right = 0; right < arry2.length; right++) {
-  if (arry2[right] == `0`) currZeros++
-  while (currZeros > 1) {
-    if (arry2[left] == `0`) currZeros--
-    left++
+// let arry2 = `1101100011`
+// let left = 0,
+//   currZeros = 0,
+//   ans = 0
+// for (let right = 0; right < arry2.length; right++) {
+//   if (arry2[right] == `0`) currZeros++
+//   while (currZeros > 1) {
+//     if (arry2[left] == `0`) currZeros--
+//     left++
+//   }
+//   ans = Math.max(ans, right - left + 1)
+//   console.log(ans)
+// }
+// console.log({ ans: ans })
+
+let arry = [10,5,2,6]
+let k = 100
+console.log(arry.length)
+
+let left = 0, curr = 1, ans = 0 
+//  for(let right = 0; right < arry.length; right++ ){
+//   curr *= arry[right]  
+//     while( curr >= k){
+//       curr /= arry[left]
+//       left++
+//     }
+//     ans += right - left + 1 
+//  }
+// console.log(ans) 
+
+for(let right = 0; right < arry.length; right++){
+  curr *= arry[right]
+  if( curr >= k){
+    curr /= arry[right]  
+    left++  
   }
-  ans = Math.max(ans, right - left + 1)
-  console.log(ans)
+  ans += right -left + 1
 }
-console.log({ ans: ans })
+console.log(ans)
