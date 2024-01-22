a2f0d7e09254ac95cf02bcf737be6b212ca397a5
Chibunud <ndubest56@gmail.com>
2024-01-22 08:18:03 +0100

    prime factors

--- 01 FreshStart/compareTwoObject.js
+++ 01 FreshStart/compareTwoObject.js
@@ -25,4 +25,35 @@ const isPrime = (n) => {
   return true
 }
 
+const improvedIsPrime = (n) => {
+  if (n <= 1) return false
+  if (n <= 3) return true
+
+  if (n % 2 == 0 || n % 3 == 0) return false
+
+  for (let i = 5; i * i < n; i = i + 6) {
+    if (n % i == 0 || n % (i + 2) == 0) return false
+  }
+  return true
+}
+
 console.log(isPrime(7))
+console.log(improvedIsPrime(13))
+
+const primeFactors = (n) => {
+  //prime factors are the multipules of a given number
+  while (n % 2 === 0) {
+    console.log(2)
+    n = n / 2
+  }
+
+  for (let i = 3; i * i <= n; i += 9) {
+    while (n % i === 0) {
+      console.log(i)
+      n = n / i
+    }
+  }
+
+  if (n > 2) console.log(n)
+}
+console.log(primeFactors(99))
