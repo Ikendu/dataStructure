136e7034e7185d0c0ba9d6ac872a959d9f74d7c7
Chibunud <ndubest56@gmail.com>
2024-01-22 05:30:33 +0100

    isPrime numbers

--- 01 FreshStart/compareTwoObject.js
+++ 01 FreshStart/compareTwoObject.js
@@ -1,4 +1,4 @@
-const compareTwoObject = (obj1, obj2) => {
+const compareTwoObjects = (obj1, obj2) => {
   let obj1Props = Object.getOwnPropertyNames(obj1)
   let obj2Props = Object.getOwnPropertyNames(obj2)
 
@@ -6,12 +6,23 @@ const compareTwoObject = (obj1, obj2) => {
 
   for (let i = 0; i < obj1Props.length; i++) {
     let props = obj1Props[i]
-    if (obj1[props] !== obj1[props]) return false
+    if (obj1[props] !== obj2[props]) return false
   }
   return true
 }
 
-console.log(
-  { a: 20, ada: `hello`, obi: `come here plz` },
-  { a: 20, ada: `hello`, obi: `come here plz` }
-)
+let first = { a: 20, ada: `hello`, obi: `come here plz` }
+let second = { bcd: 20, ada: `hello`, obi: `come here plz` }
+
+console.log(compareTwoObjects(first, second))
+
+const isPrime = (n) => {
+  //prime numbers are numbers that can be divided by one and itself
+  if (n <= 1) return false
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) return false
+  }
+  return true
+}
+
+console.log(isPrime(7))
