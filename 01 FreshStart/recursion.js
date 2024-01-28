c3b6d39d6d4ace10d9e6ff417edf45aed2f24b22
Chibunud <ndubest56@gmail.com>
2024-01-28 05:38:13 +0100

    recursion flat dictionary

--- 01 FreshStart/recursion.js
+++ 01 FreshStart/recursion.js
@@ -43,3 +43,41 @@ const betterFibu = (n, last, lasty) => {
 }
 
 console.log(betterFibu(10, 1, 0))
+
+//pascal triangle
+const pascalTri = (row, col) => {
+  if (col == 0) return 1
+  if (row == 0) return 0
+
+  return pascalTri(row - 1, col) + pascalTri(row - 1, col - 1)
+}
+console.log(`Pascal`)
+console.log(pascalTri(5, 2))
+
+//flatened Object
+const flatenedObj = (dictionary) => {
+  let flatdictionary = {}
+  const flatenedHelper = (dictionary, propName) => {
+    if (typeof dictionary != `object`) {
+      flatdictionary[propName] = dictionary
+      return
+    }
+    for (prop in dictionary) {
+      if (propName == ``) {
+        flatenedHelper(dictionary[prop], propName + prop)
+      } else {
+        flatenedHelper(dictionary[prop], propName + `.` + prop)
+      }
+    }
+  }
+
+  flatenedHelper(dictionary, ``)
+  return flatdictionary
+}
+
+const flatdic = flatenedObj({
+  key1: `valu1`,
+  key2: { key3: { key4: `valu2`, key5: `valu3`, key6: `valu4` }, key7: `valu5` },
+  key8: { key12: `valu6`, key11: { key9: `valu10` } },
+})
+console.log(flatdic)
