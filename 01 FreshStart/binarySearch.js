6adb012e25bcbde965e81d5b0adf21953ce76aa9
Chibunud <ndubest56@gmail.com>
2024-02-01 07:44:52 +0100

    binary sort sqrt

--- 01 FreshStart/binarySearch.js
+++ 01 FreshStart/binarySearch.js
@@ -1,6 +1,6 @@
 const binarySearch = (array, n) => {
   array = array.sort((a, b) => a - b)
-  console.log(array)
+  //console.log(array)
   let startIdx = 0
   let lastIdx = array.length - 1
 
@@ -20,3 +20,40 @@ const binarySearch = (array, n) => {
 }
 
 console.log(binarySearch([1, 10, 30, 2, 3, 7, 4, 5, 6], 30))
+
+//square root of a number
+const naiveSqrt = (number) => {
+  if (number == 1 || number == 0) return number
+  let square = 1
+  let index = 1
+  while (square < number) {
+    if (square == number) {
+      return square
+    }
+    index++
+    square = index * index
+  }
+  return index
+}
+console.log(naiveSqrt(0))
+
+const binarySqrt = (number) => {
+  if (number == 0 || number == 1) return number
+  let left = 1,
+    right = number,
+    ans
+  while (left <= right) {
+    let mid = parseInt((left + right) / 2)
+    let middle = mid * mid
+    if (middle == number) return mid
+    if (middle < number) {
+      left = mid - 1
+      ans = mid
+    } else {
+      right = mid + 1
+    }
+    ans = mid
+  }
+  return ans
+}
+console.log(binarySqrt(25))
