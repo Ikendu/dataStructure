167fa6ac39842c4fd1bb725b5de4d18ce869be54
Aniede Chibundu <ndubest56@gmail.com>
2024-02-12 08:03:27 +0100

    array class

--- 01 FreshStart/ArrayListClass.js
+++ 01 FreshStart/ArrayListClass.js
@@ -17,6 +17,7 @@ function List(){
     this.getElement = getElement
     this.contains = contains
 }
+
 function appendOne(ele){
     return this.listStore.push(ele)
 }
@@ -34,29 +35,57 @@ function display(){
 }
 function insertEle(ele, pos){
     this.listStore.splice(pos, 0, ele)
-
 }
 function removeEle(ele){
    let index = this.listStore.indexOf(ele)
    this.listStore.splice(index, 1)
 }
-
-function frontEle(){}
-function endEle(){}
-function prevEle(){}
-function length(){}
-function moveToPos(){}
-function getElement(){}
-function contains(){}
+function frontEle(){
+    return this.listStore[0]
+}
+function endEle(){
+    return this.listStore[this.listStore.length-1]
+}
+function prevEle(ele){
+    let index = this.listStore.indexOf(ele)
+    return this.listStore[index-1]
+}
+function length(){
+    return this.listStore.length
+}
+function moveToPos(ele, pos){
+    let currPos = this.find(ele)
+    let item = this.listStore.splice(currPos, 1)
+    this.listStore.splice(pos, 0, item)
+}
+function getElement(pos){
+    let ele = this.listStore.slice(pos-1, pos)
+    return ele.toString()
+}
+function contains(ele){
+    for(let i = 0; i < this.listStore.length; i++){
+       if(this.listStore[i] == ele) return true          
+    }
+    return false     
+}
 
 
 let myList  = new List()
 myList.append(90)
 myList.append(20)
 myList.append(45)
-myList.append(20)
+myList.append(59)
 myList.insert(30, 2)
 myList.remove(30)
-console.log(myList.find(90))
 
+
+console.log(myList.display().toString())
+console.log(myList.find(90))
+console.log(myList.front())
+console.log(myList.end())
+console.log(myList.prev(45))
+console.log(myList.length())
+myList.moveTo(90, 2)
+console.log(myList.getElement(3))
+console.log(myList.contains(45))
 console.log(myList.display().toString())
