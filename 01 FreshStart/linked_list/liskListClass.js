7c4c2aba096a8f71ace7b8690892eae3be9cbb9b
Aniede Chibundu <ndubest56@gmail.com>
2024-03-01 10:19:06 +0100

    linked list find

--- 01 FreshStart/linked_list/liskListClass.js
+++ 01 FreshStart/linked_list/liskListClass.js
@@ -4,9 +4,19 @@ function Node(element) {
 }
 
 function LinkList() {
-  this.head = new Node("first");
+  this.head = new Node("head");
   this.display = display;
   this.insert = insert;
   this.remove = remove;
+  this.find = find;
   this.traverse = traverse;
+}
+
+function find(item) {
+  let curNode = this.head;
+
+  while (curNode.element != item) {
+    curNode = curNode.next;
+  }
+  return curNode;
 }
