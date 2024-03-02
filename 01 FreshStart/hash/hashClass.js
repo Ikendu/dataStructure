c85f053d265f3c1db2c5d1f59c5a02c2dd6483a2
Aniede Chibundu <ndubest56@gmail.com>
2024-03-02 22:19:08 +0100

    insertion of hash values

--- 01 FreshStart/hash/hashClass.js
+++ 01 FreshStart/hash/hashClass.js
@@ -14,16 +14,37 @@ function hashfunc(data) {
   return total % this.table.length;
 }
 
-function insert(data) {}
+function insert(data) {
+  let pos = this.hashfunc(data);
+  this.table[pos] = data;
+}
 
 function display() {
   for (let i = 0; i < this.table.length; i++) {
     if (this.table[i]) {
-      console.log(i + "-->" + this.table[i]);
+      console.log(i + " --> " + this.table[i]);
     }
   }
 }
 
 let hasher = new Hash();
+hasher.insert("Helen");
+hasher.insert("Major");
+hasher.insert("Hero");
+var someNames = [
+  "David",
+  "Jennifer",
+  "Donnie",
+  "Raymond",
+  "Cynthia",
+  "Mike",
+  "Clayton",
+  "Danny",
+  "Jonathan",
+];
+for (let i = 0; i < someNames.length; i++) {
+  hasher.insert(someNames[i]);
+}
+hasher.display();
 
 console.log(hasher.hashfunc("boy"));
