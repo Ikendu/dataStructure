da3b3b0862294d073c96899646a74805c442b324
Aniede Chibundu <ndubest56@gmail.com>
2024-03-13 17:19:43 +0100

    redoing graph algorithm

--- 01 FreshStart/graph/graphRedo.js
+++ 01 FreshStart/graph/graphRedo.js
@@ -1,39 +1,30 @@
-function Vertix(label) {
+function Vertix(lebel) {
   this.label = label;
 }
-
 function Graph(v) {
   this.vertices = v;
   this.edges = 0;
   this.adj = [];
-    for (let i = 0; i < this.vertices; i++) {
-      this.adj[i] = [];
-      // this.adj[i].push("");
-    }
+  for (let i = 0; i < this.vertices; i++) {
+    this.adj[i] = [];
+    this.daj[i].push("");
+  }
   this.addEdges = addEdges;
   this.showGraph = showGraph;
 }
-
 function addEdges(v, w) {
   this.adj[v].push(w);
   this.adj[w].push(v);
   this.edges++;
 }
-
 function showGraph() {
   for (let i = 0; i < this.vertices; i++) {
     console.log(i + " => ");
     for (let j = 0; j < this.vertices; j++) {
-      if (this.adj[i][j]) console.log(this.adj[i][j] + " ");
+      if (adj[i][j]) {
+        console.log(adj[i][j]);
+      }
     }
     console.log();
   }
 }
-
-let g = new Graph(5);
-g.addEdges(0, 3);
-g.addEdges(3, 2);
-g.addEdges(1, 4);
-g.addEdges(2, 1);
-g.addEdges(4, 0);
-g.showGraph();
