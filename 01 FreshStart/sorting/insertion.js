707a04ffb937e0fddc6fb6f4e614e309e712df36
Aniede Chibundu <ndubest56@gmail.com>
2024-03-19 07:18:44 +0100

    insertion using while loop

--- 01 FreshStart/sorting/insertion.js
+++ 01 FreshStart/sorting/insertion.js
@@ -14,3 +14,21 @@ const insertion = (arr) => {
 };
 
 insertion(array);
+
+const insertionB = (arr) => {
+  let temp, j;
+  for (let i = 1; i < arr.length; i++) {
+    temp = arr[i];
+    j = i;
+
+    while (j > 0 && arr[j - 1] >= temp) {
+      arr[j] = arr[j - 1];
+      j--;
+    }
+    arr[j] = temp;
+  }
+  console.log(arr);
+  return arr;
+};
+
+insertionB(array);
