efa049129ec2b84a4f789a3abe38ec61cb78ea70
Aniede Chibundu <ndubest56@gmail.com>
2024-03-23 08:17:34 +0100

    more on binary search data counter

--- 01 FreshStart/search/getAllData.js
+++ 01 FreshStart/search/getAllData.js
@@ -24,14 +24,34 @@ function binary(array, data) {
         if (array[i] == data) count++;
         else break;
       }
+      console.log(`${data} is found at index ${mid}`);
       console.log(`the number of ${data} is ${count}`);
-      return `${data} is found at index ${mid}`;
+      return mid;
     }
   }
+  return -1;
 
-  return `${data} not found`;
+  //   return `${data} not found`;
 }
-let arr = [10, 20, 18, 91, 15, 20, 14, 60, 84, 20, 20, 12, 20, 50, 20, 67, 20];
-console.log(binary(arr, 20));
+let nums = [10, 20, 18, 91, 15, 20, 14, 60, 84, 20, 20, 12, 20, 50, 20, 67, 20];
+// console.log(binary(nums, 20));
 
-//getting all other instance of searched data
+//getting all other instance of searched data using function return
+
+function getOthers(arr, data) {
+  let pos = binary(arr, data);
+  if (pos > -1) {
+    let count = 1;
+    for (let i = pos - 1; i > 0; i--) {
+      if (arr[i] == data) count++;
+      else break;
+    }
+    for (let i = pos + 1; i < arr.length; i++) {
+      if (arr[i] == data) count++;
+      else break;
+    }
+    return count;
+  }
+}
+
+console.log(getOthers(nums, 20));
