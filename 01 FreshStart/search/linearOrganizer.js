bcc478f6b34016afc767faee4ceb1e35e8be26b1
Aniede Chibundu <ndubest56@gmail.com>
2024-03-23 01:16:10 +0100

    progressive move up

--- 01 FreshStart/search/linearOrganizer.js
+++ 01 FreshStart/search/linearOrganizer.js
@@ -11,3 +11,23 @@ function find(data, arr) {
   }
 }
 find(10, [40, 30, 9, 8, 11, 10, 60, 33]);
+
+//progressive organizer
+const movingUpData = (data, array) => {
+  for (let i in array) {
+    if (array[i] == data) {
+      swap(array, i, i - 1);
+      console.log(`found at index ${i}`);
+      return array;
+    }
+  }
+};
+
+const swap = (arr, i, j) => {
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+  return arr;
+};
+
+console.log(movingUpData(50, [10, 20, 5, 30, 40, 50, 35, 40, 25]));
