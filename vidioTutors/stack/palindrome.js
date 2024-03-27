8f927d7fcd09fd0de1edc5cc5ba7260997086054
Aniede Chibundu <ndubest56@gmail.com>
2024-03-27 02:26:20 +0100

    palindrome checker

---
+++ vidioTutors/stack/palindrome.js
@@ -0,0 +1,21 @@
+const palindrome = (word) => {
+  let letters = [];
+  let rword = [];
+  let length = word.length;
+
+  for (let i = 0; i < length; i++) {
+    letters.push(word[i]);
+  }
+  for (let i = 0; i < length; i++) {
+    rword += letters.pop();
+  }
+  if (rword === word) {
+    console.log(`${word} is a palindrome`);
+    return true;
+  } else {
+    console.log(`${word} is not a palindrome`);
+    return false;
+  }
+};
+
+palindrome("obi");
