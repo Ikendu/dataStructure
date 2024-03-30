5eda73793055455698ad1bc260802b8107b0459b
Aniede Chibundu <ndubest56@gmail.com>
2024-03-30 06:18:57 +0100

    string operations

---
+++ 01 FreshStart/string/string.js
@@ -0,0 +1,11 @@
+//Some string operations
+
+//charAt
+let item = "big house";
+console.log(item[1]);
+console.log(item[5]);
+console.log(item[0]);
+console.log(item.charAt(7));
+
+//substring
+console.log(item.substring(1, 6));
