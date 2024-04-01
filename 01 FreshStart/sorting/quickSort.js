8ae6d7f961199182296f92884b94eec4253f7ab2
Aniede Chibundu <ndubest56@gmail.com>
2024-04-01 11:42:29 +0100

    quicksort redone

--- 01 FreshStart/sorting/quickSort.js
+++ 01 FreshStart/sorting/quickSort.js
@@ -9,8 +9,8 @@ const quickSort = (array) => {
     if (array[i] < pivot) lower.push(array[i]);
     else higher.push(array[i]);
   }
-    let result = quickSort(lower).concat(pivot, quickSort(higher));
-    return result
+  let result = quickSort(lower).concat(pivot, quickSort(higher));
+  return result;
 };
 
 function creatList(num) {
@@ -24,3 +24,22 @@ function creatList(num) {
 let newList = creatList(10000);
 // console.log(newList);
 console.log(quickSort(newList));
+
+// Revision
+function quicksort(array) {
+  if (array.length == 0) {
+    return [];
+  }
+  let lesser = [];
+  let higher = [];
+  let pivot = array[0];
+
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < pivot) lesser.push(array[i]);
+    else higher.push(array[i]);
+  }
+
+  return quicksort(lesser).concat(pivot, quickSort(higher));
+}
+
+console.log(quicksort([10, 3, 0, 8, 11, 5, 9, 4]));
